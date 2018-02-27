556a290303d046378c4d5832b2bd44a1a65e85fd
Robert Gevorgyan <robert1999.g@gmail.com>
2018-02-27 16:20:53 +0000

    Add index.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+const sumInt = (a, b) => {}
+const subtractionInt = (a, b) => {}
+const divisionInt = (a, b) => {}
+const multiplicationInt = (a, b) => {}
+const power = (a, pow) => {}
+const root = (a, pow) => {}
+const absolute = (a) => {}
+const mod = (a, b) => {}
+const log = (a, base) => {}
+const flipSign = (a) => {}
+const sin = (a) => {}
+const cos = (a) => {}
+const percent = (a, b) => {}
+const integral = (func, a, b, precision) => {}
